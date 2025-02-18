fe487414fe0693d8acf738aeb45e7e1d31caa986
harshthakkar09” git config --global user.email “thakkarharsh1999@gmail.com <harthakk@Harshs-MacBook-Pro.local>
2025-02-18 22:06:14 +0530

    Added login component

---
+++ src/components/Login.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="card p-4 shadow" style={{ width: "400px" }}>
+        <h3 className="text-center">Login</h3>
+        <form>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Enter email" />
+          </div>
+          <div className="form-group mt-3">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+          </div>
+          <button type="submit" className="btn btn-primary w-100 mt-3">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
